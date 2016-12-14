075cb9a6e2932975ecb1ac0bc4d13920636e8275
Jacek Milewski <jacek.milewski@orange.com>
2016-12-14 14:10:53 +0100

    Initial commit

---
+++ src/main/java/com/looksok/tdd/TddDemoApplication.java
@@ -0,0 +1,12 @@
+package com.looksok.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddDemoApplication.class, args);
+	}
+}
