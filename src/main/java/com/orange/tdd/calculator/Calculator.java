3cd0caf1ccfee1db441d8574d02b0900717f8b77
Jacek Milewski <jacek.milewski@orange.com>
2016-12-14 14:27:11 +0100

    calculator test

---
+++ src/main/java/com/orange/tdd/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.orange.tdd.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/orange/tdd/calculator/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.orange.tdd.calculator;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    public void addsTwoIntegers() throws Exception {
+        //given
+        Calculator calculator = new Calculator();
+
+        //then
+        assertThat(calculator.add(1, 2)).isEqualTo(3);
+        assertThat(calculator.add(21, 2)).isEqualTo(23);
+        assertThat(calculator.add(21, 0)).isEqualTo(21);
+        assertThat(calculator.add(21, -10)).isEqualTo(11);
+    }
+}
